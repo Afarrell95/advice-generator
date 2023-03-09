9e652eebb4b63a5f29e868a2d5ae8d6fe9138884
Allison <farrella95@gmail.com>
2023-03-09 17:32:17 -0500

    api functionallity

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 function getFetch() {
   const url = `	https://api.adviceslip.com/advice`;
   fetch(url)
-    .then((res) => res.json()) // parse response as JSON
+    .then((res) => res.json())
     .then((data) => {
-      console.log(data);
+      console.log(data.slip.id);
+      console.log(data.slip.advice);
 
       document.querySelector(
-        "advice__number"
-      ).innerHTML = `Advice #${data.slip.id}`;
-      document.querySelector("advice__text").innerHTML = `${data.slip.advice}`;
+        ".advice__number"
+      ).innerHTML = `ADVICE #${data.slip.id}`;
+      document.querySelector(
+        ".advice__text"
+      ).innerHTML = `"${data.slip.advice}"`;
     })
 
     .catch((err) => {
