1095bbd88a484606b5821be0fe9489ded5064e68
Allison <farrella95@gmail.com>
2023-03-15 18:06:09 -0400

    added project

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+let button = document.querySelector(".circle");
+
 function getFetch() {
   const url = `	https://api.adviceslip.com/advice`;
   fetch(url)
@@ -24,7 +26,5 @@ getFetch();
 function refreshPage() {
   window.location.reload();
 }
-
-let button = document.querySelector(".circle");
 
 button.addEventListener("click", refreshPage);
